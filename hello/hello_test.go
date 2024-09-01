ed60ab9accfb2160944d96cedd20e3012ccae479
Corey Burns <coreyburns@outlook.com>
2024-09-01 16:44:23 -0400

    {1:-Updated Dependencies}

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,25 +2,25 @@ package main
 
 import "testing"
 
-func TestHello(q *testing.T) {
-	q.Run("says hello when given name", func(t *testing.T) {
+func TestHello(t *testing.T) {
+	t.Run("says hello when given name", func(t *testing.T) {
 		got := Hello("Corey")
-		want := "Hello, Corey"
+		want := "Hello, Coray"
 
 		assertCorrectMessage(t, got, want)
 
 	})
-	q.Run("says hello world when no name given", func(t *testing.T) {
+	t.Run("says hello world when no name given", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
 		assertCorrectMessage(t, got, want)
 	})
 }
 
-func assertCorrectMessage(q testing.TB, got, want string) {
-	q.Helper()
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		q.Errorf("got %s want %s", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 
 }
