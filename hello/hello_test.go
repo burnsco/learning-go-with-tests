2d41869d71ca98a95c5daf91261551863c5a2b59
Corey Burns <coreyburns@outlook.com>
2024-09-01 16:36:34 -0400

    added test helper function 'assertCorrectMessage' for optimal testing

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,19 +7,20 @@ func TestHello(q *testing.T) {
 		got := Hello("Corey")
 		want := "Hello, Corey"
 
-		if got != want {
-			q.Errorf("got %s want %s", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 
 	})
 	q.Run("says hello world when no name given", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			q.Errorf("got %s want %s", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(q testing.TB, got, want string) {
+	q.Helper()
+	if got != want {
+		q.Errorf("got %s want %s", got, want)
+	}
 
 }
