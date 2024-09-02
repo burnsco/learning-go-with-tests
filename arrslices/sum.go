3855abfd3431e4ed324676bdfe6d430d80bd0667
Corey Burns <coreyburns@outlook.com>
2024-09-02 11:49:03 -0400

    slices are better!

--- arrslices/sum.go
+++ arrslices/sum.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
-
 func Sum(arr []int) int {
 	sum := 0
 	for _, value := range arr {
 		sum += value
 	}
 	return sum
-}
-
-func main() {
-	exampleArray := []int{1, 3, 4, 5, 1, 2, 3}
-	fmt.Println("Array elements: ", exampleArray)
-	fmt.Println("Sum of elements: ", Sum(exampleArray))
 }
