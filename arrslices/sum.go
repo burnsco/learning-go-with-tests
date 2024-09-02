805fd06c9a0c6f7f2be6f56896b701114e70c806
Corey Burns <coreyburns@outlook.com>
2024-09-02 12:20:31 -0400

    added a sumall function, getting length of the slices first, then making new array for the final sums, then looping through each and returning total

--- arrslices/sum.go
+++ arrslices/sum.go
@@ -1,9 +1,29 @@
 package main
 
+import "fmt"
+
 func Sum(arr []int) int {
 	sum := 0
 	for _, value := range arr {
 		sum += value
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func main() {
+	arr1 := []int{1, 3, 3}
+	arr2 := []int{4, 5, 6}
+
+	fmt.Println(SumAll(arr1, arr2))
 }
