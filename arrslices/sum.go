5acd61c2b74a906cded2021c6e1fa883850ea3c0
Corey Burns <coreyburns@outlook.com>
2024-09-02 12:59:58 -0400

    messing around with slices, going off course to learn 2d and 3d slices

--- arrslices/sum.go
+++ arrslices/sum.go
@@ -7,21 +7,28 @@ func Sum(arr []int) int {
 	for _, value := range arr {
 		sum += value
 	}
+
 	return sum
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
 }
 
 func main() {
+	var slice []int
+
+	slice = append(slice, 1, 2, 3)
+	fmt.Println("Slice after first append:", slice, "Capacity:", cap(slice))
+
+	slice = append(slice, 5, 6, 7)
+	fmt.Println("Slice after secondappend:", slice, "Capacity:", cap(slice))
+
 	arr1 := []int{1, 3, 3}
 	arr2 := []int{4, 5, 6}
 
